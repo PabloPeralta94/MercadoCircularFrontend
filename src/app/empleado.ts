bcefe09101d3658cf4933a7eba155688539fecfc
GastonPeralta93 <gastonperalta93@gmail.com>
2023-07-17 02:24:41 -0300

    commit inicial app base

---
+++ src/app/empleado.ts
@@ -0,0 +1,12 @@
+export interface Empleado {
+    id: number;
+
+    nombre: string;
+    apellido: string;
+    email: string;
+    telefono: string;
+    
+    puestoDeTrabajo: string;
+    imageUrl: string;
+    codigoEmpleado: string;
+}
