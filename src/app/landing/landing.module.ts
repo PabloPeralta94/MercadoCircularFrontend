2ec6ca5e8f60a3caa843d21ca953cbebb75ffca6
PabloPeralta94 <pablo.peralta94@gmail.com>
2023-07-20 23:14:29 -0300

    creacion del landing component para las opciones de registro y login

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LogueoComponent } from './logueo/logueo.component';
 import { RegistroComponent } from './registro/registro.component';
+import { MainComponent } from './main.component';
 
 
 
 @NgModule({
   declarations: [
     LogueoComponent,
-    RegistroComponent
+    RegistroComponent,
+    MainComponent
   ],
   imports: [
     CommonModule
