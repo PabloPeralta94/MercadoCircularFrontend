ab727b23d9207756b29e216bec6d4531b1ab3621
PabloPeralta94 <pablo.peralta94@gmail.com>
2023-07-23 21:49:06 -0300

    configuracion de servicios e interfaces para auth

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NuevoUsuario } from '../interfaces/nuevo-usuario';
+import { Observable } from 'rxjs';
+import { LoginUsuario } from '../interfaces/login-usuario';
+import { JwtDTO } from '../interfaces/jwt-dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  authURL = 'http://localhost:8080/auth/';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  public nuevo(nuevoUsuario: NuevoUsuario): Observable<any> {
+    return this.httpClient.post<any>(this.authURL + 'nuevo', nuevoUsuario);
+  }
+
+  public login(loginUsuario: LoginUsuario): Observable<JwtDTO> {
+    return this.httpClient.post<JwtDTO>(this.authURL + 'login', loginUsuario);
+  }
 }
